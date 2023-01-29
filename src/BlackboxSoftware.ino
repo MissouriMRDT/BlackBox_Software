344a7425b2f6a3b0689c1d6b12b36d7224b2c78d
Grant Brinker <gsbfy2@umsystem.edu>
2023-01-28 22:50:16 -0600

    Worked on board struct

--- src/BlackboxSoftware.ino
+++ src/BlackboxSoftware.ino
@@ -13,12 +13,35 @@ void setup()
 
     telemetry.begin(Telemetry, 1500000);
     Serial.println("Started: ");
-    
+
+    struct Board BMS, PowerBoard, Nav, ArmBoard, SciMoco, SciHeater, SciSensors;
+
+    BMS.boardIP =                   "192.168.1.133";
+    PowerBoard.boardIP =            "192.168.1.132";
+    Nav.boardIP =                   "192.168.1.136";
+    ArmBoard.boardIP =              "192.168.1.131";
+    SciMoco.boardIP =               "192.168.1.137";
+    SciHeater.boardIP =             "192.168.1.144";
+    SciSensors.boardIP =            "192.168.1.138";
+
+    BMS.boardFourthOctet =          133;
+    PowerBoard.boardFourthOctet =   132;
+    Nav.boardFourthOctet =          136;
+    ArmBoard.boardFourthOctet =     131;
+    SciMoco.boardFourthOctet =      137;
+    SciHeater.boardFourthOctet =    144;
+    SciSensors.boardFourthOctet =   138;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void loop()
 {
+    packet = RoveComm.read();
 
+    switch(packet.data_id)
+    {
+        case RC_BLACKBOXBOARD_BLACKBOXLISTENING_DATA_ID:
+            
+    }
 }
